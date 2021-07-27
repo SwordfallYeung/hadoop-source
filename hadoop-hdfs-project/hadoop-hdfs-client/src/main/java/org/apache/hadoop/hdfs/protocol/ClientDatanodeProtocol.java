7710e8daf15846f012918b1147e794f9096359b0
Swordfall <2506597416@qq.com>
2021-07-28 01:11:02 +0800

    ClientDatanodeProtocol部分方法注释说明

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/ClientDatanodeProtocol.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/protocol/ClientDatanodeProtocol.java
@@ -33,6 +33,12 @@ import org.apache.hadoop.security.token.TokenInfo;
 import org.apache.hadoop.hdfs.server.datanode.DiskBalancerWorkStatus;
 
 /** An client-datanode protocol for block recovery
+ *
+ * todo ClientDatanodeProtocol定义了Client与Datanode之间的接口
+ *
+ * todo ClientDatanodeProtocol中定义的接口可以分为两部分：
+ *    一部分是支持HDFS文件读取操作的，例如getReplicaVisibleLength()以及getBlockLocalPathInfo();
+ *    另一部分是支持DFSAdmin中与数据节点管理相关的命令。
  */
 @InterfaceAudience.Private
 @InterfaceStability.Evolving
@@ -71,6 +77,10 @@ public interface ClientDatanodeProtocol {
    * Adds new namenodes and stops the deleted namenodes.
    *
    * @throws IOException on error
+   *
+   * todo 在用户管理员命令中有一个‘hdfs dfsadmin datanodehost:port’命令，
+   *      用于触发指定的Datanode重新加载配置文件，停止服务那些已经从配置文件中删除
+   *      的块池（blockPool），开始服务新添加的块池。
    **/
   void refreshNamenodes() throws IOException;
 
@@ -83,6 +93,16 @@ public interface ClientDatanodeProtocol {
    *          i.e. if it doesn't contain any block files, otherwise it is
    *          deleted along with its contents.
    * @throws IOException
+   *
+   * todo 用于从指定Datanode删除blockpoolId对应的块池，如果force参数被设置了，
+   *    那么无论这个块池目录中有没有数据都会被强制删除；否则，只有这个块池目录为空
+   *    的情况下才会被删除。
+   *
+   * todo 如果Datanode还在服务这个块池，这个命令的执行将会失败。要停止一个数据节点
+   *    指定的块池，需要调用上面提到的refreshNamenodes()方法。
+   *
+   * todo deleteBlockPool()方法有两个参数，其中blockpoolId用于设置要被删除的块池ID;
+   *      force用于设置是否强制删除。
    */
   void deleteBlockPool(String bpid, boolean force) throws IOException;
 
@@ -107,6 +127,14 @@ public interface ClientDatanodeProtocol {
    * @return the BlockLocalPathInfo of a block
    * @throws IOException
    *           on error
+   *
+   * todo HDFS对于本地读取，也就是Client和保存该数据块的Datanode在同一台物理机器上时，
+   *    是有很多优化的。Client会调用ClientProtocol.getBlockLocalPathInfo()方法
+   *    获取指定数据块文件以及数据块校验文件在当前节点上的本地路径，然后利用这个本地路径
+   *    执行本地读取操作，而不是通过流式接口执行远程读取，这样也就大大优化了。
+   *
+   * todo 客户端会通过调用DataTransferProtocol接口从数据节点获取数据块文件的文件描述符，
+   *    然后打开并读取文件以实现短路读操作，而不是通过ClientDatanodeProtocol接口。
    */
   BlockLocalPathInfo getBlockLocalPathInfo(ExtendedBlock block,
       Token<BlockTokenIdentifier> token) throws IOException;
@@ -119,6 +147,8 @@ public interface ClientDatanodeProtocol {
    *          certain states for quick restart. This should only be used when
    *          the stored data will remain the same during upgrade/restart.
    * @throws IOException
+   *
+   * todo 用于关闭一个数据节点，这个方法主要是为了支持管理命令
    */
   void shutdownDatanode(boolean forUpgrade) throws IOException;
 
@@ -133,17 +163,25 @@ public interface ClientDatanodeProtocol {
    * Obtains datanode info
    *
    * @return software/config version and uptime of the datanode
+   *
+   * todo 用于获取指定Datanode的信息，这里的信息包括Datanode运行的HDFS版本、Datanode配置的HDFS版本，
+   *    以及Datanode的启动事件，对应于管理命令‘hdfs dfsadmin-getDatanodeInfo’
    */
   DatanodeLocalInfo getDatanodeInfo() throws IOException;
 
   /**
    * Asynchronously reload configuration on disk and apply changes.
+   *
+   * todo 用于触发Datanode异步地从磁盘重新加载配置，并且应用该配置。
+   *      这个方法用于支持管理命令‘hdfs dfsadmin-getDatanodeInfo-reconfigstart’
    */
   void startReconfiguration() throws IOException;
 
   /**
    * Get the status of the previously issued reconfig task.
    * @see org.apache.hadoop.conf.ReconfigurationTaskStatus
+   *
+   * todo 查询上一次触发的重新加载配置操作的运行情况
    */
   ReconfigurationTaskStatus getReconfigurationStatus() throws IOException;
 
@@ -167,6 +205,8 @@ public interface ClientDatanodeProtocol {
 
   /**
    * Get volume report of datanode.
+   *
+   * todo 用于获取数据块是存储在指定Datanode的哪个卷（volume）上的
    */
   List<DatanodeVolumeInfo> getVolumeReport() throws IOException;
 
