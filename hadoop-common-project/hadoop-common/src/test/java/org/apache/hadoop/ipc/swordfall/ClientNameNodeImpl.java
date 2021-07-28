f3af994a14d54cb0e50043d0c6c6fd568a8f96a8
Swordfall <2506597416@qq.com>
2021-07-29 00:58:28 +0800

    Hadoop RPC三部分说明，1.协议，2.server, 3.client

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/swordfall/ClientNameNodeImpl.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/swordfall/ClientNameNodeImpl.java
@@ -1,6 +1,8 @@
 package org.apache.hadoop.ipc.swordfall;
 
 /**
+ * todo 第一部分协议实现：实现协议这个也很简单，就是根据接口创建一个实现类，用的时候注册到Server服务中即可。
+ *
  * 实现协议结构
  */
 public class ClientNameNodeImpl implements ClientNameNodeProtocol {
--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/swordfall/ClientNameNodeProtocol.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/swordfall/ClientNameNodeProtocol.java
@@ -1,6 +1,9 @@
 package org.apache.hadoop.ipc.swordfall;
 
 /**
+ * todo 在这里RPC实现其实就是分三部分，分别是协议定义（接口）&实现，Server端实现和Client实现三个部分。
+ *
+ * todo 第一部分定义协议：其实就是根据业务需要定义个接口协议。
  * 协议接口
  */
 public interface ClientNameNodeProtocol {
